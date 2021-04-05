34e83d0bd170d3359b80cd6d05fe106dfdf26018
yuki-takara <tkra.yuuki@gmail.com>
2021-04-06 04:56:10 +0900

    feat: Storybookの各種機能を試す
    - addon controls
    - storyName
    - Decorators

--- components/LinkButton.stories.tsx
+++ components/LinkButton.stories.tsx
@@ -5,15 +5,29 @@ import LinkButton, { LinkButtonProps } from './LinkButton'
 
 export default {
   title: 'Design System/Atoms/Button',
-  component: LinkButton
+  component: LinkButton,
+  argTypes: {
+    bgc: { control: 'color' },
+  },
 } as Meta
 
-const Template: Story<LinkButtonProps> = (args) => <LinkButton {...args} />
+const Template: Story<LinkButtonProps> = args => <LinkButton {...args} />
 
 export const DefaultButton = Template.bind({})
 DefaultButton.args = {}
+DefaultButton.storyName = 'デフォルトのボタン'
+DefaultButton.decorators = [
+  Story => (
+    <div
+      style={{ backgroundColor: '#000', height: '100px', display: 'flex', alignItems: 'center' }}
+    >
+      <Story />
+    </div>
+  ),
+]
 
 export const RedButton = Template.bind({})
 RedButton.args = {
-  bgc: '#f00'
+  bgc: '#f00',
 }
+RedButton.storyName = '赤いボタン'
